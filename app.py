f1fe02bfc5b397859523e6020dcba835a50c2060
23542 <23542@wc.school.nz>
2026-09-22 12:26:54 +1200

    finished database + 2nd one which I need to import it into

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "umamusume.db"
 
+def create_connection(db_file):
+   try:
+       connection = sqlite3.connect(db_file)
+       return connection
+   except Error as e:
+       print(e)
+   return None
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_home():
+   return render_template("index.html")
 
+@app.route('/characters')
+def render_cards():
+    query = " FROM "
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    cur.execute(query)
+    card_list = cur.fetchall()
+    con.close()
+    return render_template("cards.html",cards=character_list)
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/horses')
+def render_cards():
+    query = " FROM "
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    cur.execute(query)
+    card_list = cur.fetchall()
+    con.close()
+    return render_template("horses.html",cards=horse_list)
